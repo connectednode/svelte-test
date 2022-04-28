fcf0434607ea27e3fb5bd5111a76b64b0995b0dd
tyco <grapefruit@posteo.org>
2022-04-28 15:19:33 +0200

    initial commit for blackjack

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Card from "./card.svelte";
+</script>
+
+<main>
+	<Card></Card>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/card.svelte
@@ -0,0 +1,26 @@
+<script>
+    let card = {
+        suit : "♦",
+        face: ""
+    }
+
+
+</script>
+
+<style>
+    div {
+        border-style: solid;
+        border-width: 10px;
+        border-color: black;
+        border-radius: 30px;
+        height: 92mm;
+        width: 59mm;
+    }
+</style>
+
+<div>
+    &#9824;
+    &#9829;
+    &#9827;
+    {card.suit}
+</div>
